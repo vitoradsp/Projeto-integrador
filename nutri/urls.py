707a8fd6d3927f3c8ec7ed7caf8631a53a9fe7e3
Vitor <vitoradsp@gmail.com>
2022-05-25 15:37:11 -0300

    alt url

--- nutri/urls.py
+++ nutri/urls.py
@@ -2,11 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns =[
     path('',views.index,name='home'),
-<<<<<<< HEAD
-    path('login/', views.UserLogin, name='login'),
-=======
     path('login/',views.UserLogin, name='login'),
     path('introduçâo/',views.introducao, name='introdução'),
     path('criando_sua_dieta/',views.create_diet, name='criar_dieta'),
->>>>>>> fabian
 ]
